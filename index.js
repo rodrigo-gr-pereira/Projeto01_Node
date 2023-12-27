160e4f9c6dba18d9c33bcc8aa6c9ee41edb4871c
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2023-12-27 18:21:15 -0300

    2

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/',(req,res)=>{
 
-    res.send('carregado pagina inicial!');
-    
+    res.render('index', {nome: 'Rodrigo'});
+
 });
 
 app.listen(5000,()=>{
