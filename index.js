ba3f1d417c42d13e666b71e1ed7dc5e3b5d484f3
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2023-12-27 21:36:23 -0300

    3
    add metodo deletar

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
+var tarefas = ['Arrumar o quarto ', ' Comprar no supermercado'];
+
 app.get('/',(req,res)=>{
 
-    res.render('index', {nome: 'Rodrigo'});
+    res.render('index', {tarefasList:tarefas}) ;
 
 });
+
+app.get('/deletar/:id',(req,res)=>{
+    tarefas = tarefas.filter(function(val,index){
+        if(index != req.params.id){
+            return val;
+        }
+        res.render('index', {tarefasList:tarefas}) ;
+    })
+})
 
 app.listen(5000,()=>{
     console.log('servidor rodando!');
