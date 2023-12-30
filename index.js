7aac1ea17570d5658aa1c433636df20ede71d274
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2023-12-30 07:37:08 -0300

    final

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use( bodyParser.json() );     //to suport JSON-encoded bodies
+app.use(bodyParser.urlencoded({  //to suport URL-encoded bodies
+ extended: true
+}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -9,6 +15,11 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
 var tarefas = ['Arrumar o quarto ', ' Comprar no supermercado'];
+
+app.post('/',(req,res)=>{
+    tarefas.push(req.body.tarefa);
+    res.render('index', {tarefasList:tarefas}) ;
+})
 
 app.get('/',(req,res)=>{
 
@@ -21,7 +32,7 @@ app.get('/deletar/:id',(req,res)=>{
         if(index != req.params.id){
             return val;
         }
-        res.render('index', {tarefasList:tarefas}) ;
+        res.render('index', {tarefasList:tarefas});
     })
 })
 
